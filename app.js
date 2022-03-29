69521c8c4be97672e81534909df866d551f02018
Victor Bondarenko <nirasimha@gmail.com>
2022-03-29 16:41:32 -0400

    add html var

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const tick = () => {
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
   // console.log(hours, minutes, seconds);
+  const html = `
+  <span>${hours}</span>
+  <span>${minutes}</span>
+  <span>${seconds}</span>
+  `;
+
+  clock.innerHTML = html;
 };
 
 setInterval(tick, 1000);
