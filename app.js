68453d4e2d25bb7040bf1fbb9cc8f285a382f25d
Victor Bondarenko <nirasimha@gmail.com>
2022-03-29 16:25:27 -0400

    set interval to show tick every second

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const clock = document.querySelector('.clock');
 
 const tick = () => {
   const now = new Date();
+
+  const hours = now.getHours();
+
+  console.log(hours);
 };
+
+setInterval(tick, 1000);
